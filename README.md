537a679b1c3e4573e313eacb4c25921b51c99943
otegami <otegami@clear-code.com>
2024-07-04 15:58:21 +0900

    Simplify the example code

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # sandbox-github-actions-artifacts-v4
 
 ```console
-$ ARTIFACT_URL="https://api.github.com/repos/otegami/sandbox-github-actions-artifacts-v4/actions/artifacts/:artifact_id" \
+$ ARTIFACT_URL="https://api.github.com/repos/:owner/sandbox-github-actions-artifacts-v4/actions/artifacts/:artifact_id/zip" \
   GITHUB_ACCESS_TOKEN=xxxx \
   ruby download_artifact.rb
 ```
+
+## Note
+
+We dont't need `/zip` but we want to make it redirect so we added it here.
+ref: https://docs.github.com/en/rest/actions/artifacts?apiVersion=2022-11-28#get-an-artifact
--- download_artifact.rb
+++ download_artifact.rb
@@ -1,35 +1,22 @@
 require 'open-uri'
 
-url = ENV['ARTIFACT_URL']
+uri = ENV['ARTIFACT_URL']
 access_token = ENV['GITHUB_ACCESS_TOKEN']
 
-# Error case
-# URI(url).open("Authorization" => "token #{access_token}") do |artifact|
-#   File.open("artifact.yml", "wb") do |file|
-#     file.write(artifact.read)
-#   end
-# end
-
-# Error case when using redirect false
-# Success case when using redirect true
 headers = {
   "Authorization" => "token #{access_token}",
   redirect: false
 }
 
-uri = URI(url)
 response = nil
 
 loop do
   begin
-    response = URI.open(uri.to_s, headers)
+    response = URI.open(uri, headers)
     break
   rescue OpenURI::HTTPRedirect => redirect
-    uri = redirect.uri
-    headers = {}
+    pp uri = redirect.uri
   end
 end
 
-File.open("artifact.yml", "wb") do |file|
-	file.write(response.read)
-end
+pp response

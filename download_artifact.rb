6cf95a61f0d23f8a823c6a6cf6b01421b015db8e
otegami <otegami@clear-code.com>
2024-07-16 15:28:59 +0900

    Update exmaple codes

--- download_artifact.rb
+++ download_artifact.rb
@@ -3,20 +3,21 @@ require 'open-uri'
 uri = ENV['ARTIFACT_URL']
 access_token = ENV['GITHUB_ACCESS_TOKEN']
 
+# Failed example
+# URI(uri).open("Authorization" => "token #{access_token}")
+
+# succeeded example
 headers = {
-  "Authorization" => "token #{access_token}",
-  redirect: false
+ "Authorization" => "token #{access_token}",
+ redirect: false
 }
 
-response = nil
-
 loop do
-  begin
-    response = URI.open(uri, headers)
-    break
-  rescue OpenURI::HTTPRedirect => redirect
-    pp uri = redirect.uri
-  end
+ begin
+   URI.open(uri, headers)
+   break
+ rescue OpenURI::HTTPRedirect => redirect
+   headers.delete("Authorization")
+   pp uri = redirect.uri
+ end
 end
-
-pp response
